d1d7bac9e5c73ce9c31601e17f30324c216ebaac
DEVENDRA MAURYA <dmaurya292@gmail.com>
2025-12-02 00:24:53 +0530

    project restructure

--- ProductService.Domain/Repositories/IInventoryRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductService.Domain.Repositories
-{
-    public interface IInventoryRepository
-    {
-        Task<bool> IsStockAvailableAsync(Guid productId, int quantity);
-        Task UpdateStockAsync(Guid productId, int stockQuantity);
-        Task IncreaseDecsreaseStockAsync(Guid productId, int quantityChange);
-    }
-}
